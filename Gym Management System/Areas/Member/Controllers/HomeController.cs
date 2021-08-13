a366bba9bf7f43aa9c53220d1b91cb7cf3f87508
Xavy Urbina <xavyurbina88@gmail.com>
2021-08-12 18:20:02 -0600

    Redesing the login page

--- Gym Management System/Areas/Member/Controllers/HomeController.cs
+++ Gym Management System/Areas/Member/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using Gym_Management_System.Models;
+﻿using GMS.DataAccess.Data.Repository.IRepository;
+using Gym_Management_System.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Gym_Management_System.Controllers
@@ -12,14 +14,21 @@ namespace Gym_Management_System.Controllers
     [Area("Member")]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IndexMember()
         {
             return View();
         }
@@ -34,5 +43,15 @@ namespace Gym_Management_System.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region API Call
+        public IActionResult GetAll()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            return Json(new { data = _unitOfWork.Order.GetAll(u => u.Client.Email == claims.Value, includeProperties: "Client,Membership,Period,Shift") });
+        }
+        #endregion
     }
 }
